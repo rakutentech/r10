1910f72af696651d16e9026e72f9207094ca698a
Luca <lucamug@gmail.com>
2022-07-15 23:13:10 +0900

    Version bump 6.1.0

--- src/R10/Svg/LogosExtra.elm
+++ src/R10/Svg/LogosExtra.elm
@@ -1,8 +1,8 @@
-module R10.Svg.LogosExtra exposing (apple, apple_monochrome, elm, elm_monochrome, facebook, facebook_monochrome, github, google, google_monochrome, microsoft, microsoft_monochrome, pcHome24hours, pcHome24hours_monochrome, r10, visa, visa_monochrome, americanExpress_monochrome, americanExpress, cardVisa, cardMasterCard, cardDiscover, cardJCB, cardAmericanExpress, cardDiners)
+module R10.Svg.LogosExtra exposing (apple, apple_monochrome, elm, elm_monochrome, facebook, facebook_monochrome, github, google, google_monochrome, microsoft, microsoft_monochrome, pcHome24hours, pcHome24hours_monochrome, r10, visa, visa_monochrome, americanExpress_monochrome, americanExpress, cardVisa, cardMasterCard, cardDiscover, cardJCB, cardAmericanExpress, cardDiners, facebookMeta)
 
 {-|
 
-@docs apple, apple_monochrome, elm, elm_monochrome, facebook, facebook_monochrome, github, google, google_monochrome, microsoft, microsoft_monochrome, pcHome24hours, pcHome24hours_monochrome, r10, visa, visa_monochrome, americanExpress_monochrome, americanExpress, cardVisa, cardMasterCard, cardDiscover, cardJCB, cardAmericanExpress, cardDiners
+@docs apple, apple_monochrome, elm, elm_monochrome, facebook, facebook_monochrome, github, google, google_monochrome, microsoft, microsoft_monochrome, pcHome24hours, pcHome24hours_monochrome, r10, visa, visa_monochrome, americanExpress_monochrome, americanExpress, cardVisa, cardMasterCard, cardDiscover, cardJCB, cardAmericanExpress, cardDiners, facebookMeta
 
 -}
 
@@ -168,9 +168,20 @@ facebook_monochrome attrs cl size =
 
 
 {-| -}
+facebookMeta : List (Attribute (R10.Context.ContextInternal z) msg) -> Int -> Element (R10.Context.ContextInternal z) msg
+facebookMeta attrs size =
+    R10.Svg.Utils.wrapperWithViewbox attrs
+        "0 0 14222 14222"
+        size
+        [ Svg.path [ SA.d "M14222 7111C14222 3184 11038 0 7111 0S0 3184 0 7111c0 3549 2600 6491 6000 7025V9167H4194V7111h1806V5544c0-1782 1062-2767 2686-2767 778 0 1592 139 1592 139v1750h-897c-883 0-1159 548-1159 1111v1334h1972l-315 2056H8222v4969c3400-533 6000-3475 6000-7025z", SA.style "fill-rule: nonzero; fill: rgb(255, 255, 255);" ] []
+        , Svg.path [ SA.d "m9879 9167 315-2056H8222V5777c0-562 275-1111 1159-1111h897V2916s-814-139-1592-139c-1624 0-2686 984-2686 2767v1567H4194v2056h1806v4969c362 57 733 86 1111 86s749-30 1111-86V9167h1657z", SA.style "fill-rule: nonzero; fill: rgba(0, 0, 0, 0);" ] []
+        ]
+
+
+{-| -}
 apple : List (Attribute (R10.Context.ContextInternal z) msg) -> Int -> Element (R10.Context.ContextInternal z) msg
 apple attrs size =
-    apple_monochrome attrs (R10.Color.Utils.fromHex "#555") size
+    apple_monochrome attrs (R10.Color.Utils.fromHex "#FFF") size
 
 
 {-| -}
